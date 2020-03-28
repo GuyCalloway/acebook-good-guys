cc0764ebcd3da9cdb6e32510464dd1a628e0dfdf
GuyCalloway <guyfcalloway@gmail.com>
2020-03-28 14:22:31 +0000

    Fix: gem versions

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Acebook
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 5.1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_135443) do
+ActiveRecord::Schema.define(version: 2019_09_19_135_443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
