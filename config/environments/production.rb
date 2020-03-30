278bd433243a00f850c88cc4ee54c0354128e76e
GuyCalloway <guyfcalloway@gmail.com>
2020-03-30 08:31:24 +0100

    Local Debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
   config.read_encrypted_secrets = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
