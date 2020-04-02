bf91fb27c093a2aab98d3410c677e068d8d35736
GuyCalloway <guyfcalloway@gmail.com>
2020-04-02 10:15:08 +0100

    Update: LikesController backend already_liked check

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,8 +12,11 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like.destroy
-    redirect_to post_path(@post)
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
   end
 
   private
@@ -25,5 +28,10 @@ class LikesController < ApplicationController
   def find_like
     @like = @post.likes.find(params[:id])
  end
+
+ def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).any? 
+  end
 
 end
